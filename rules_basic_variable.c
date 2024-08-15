96036ce1ab8da4fb1f7e59ee28bfe56d8364a64d
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-08-15 01:37:34 -0300

    arrumando

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -2,16 +2,16 @@
 
 // ..
 
-int a = 0; // var global
-int b = 0; // var global
 
-void foo(void) {
+void foo(int a) {
     a = a + 1; // acessa variavel global
 }
 
 void main(void) {
+    int a = 0; // var global
+    int b = 0; // var global
     while (1) {
-        foo();
+        foo(a);
         
         if (a > 5) {
             b = 1;
